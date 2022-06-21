4ada5f708257233abf08a548e3a23047426a0283
wuuconix <1521900139@qq.com>
2022-06-21 18:47:48 +0800

    even day do acid

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ scheduleJob('0 8 * * *', () => {
 })
 
 scheduleJob('0 17 * * *', () => {
-    sendGroupMessage({ target: xiongyue, messageChain:[{ type:"Plain", text: `兄弟们，准备做核酸啦\n${acid} 填写问卷`}] })
-    console.log("bingo 每日核酸发送成功")
+    const day = new Date().getDate()
+    if (day % 2 == 0) { //偶数天做核算
+        sendGroupMessage({ target: xiongyue, messageChain:[{ type:"Plain", text: `兄弟们，准备做核酸啦\n${acid} 填写问卷`}] })
+        console.log("bingo 每日核酸发送成功")
+    } else {
+        sendGroupMessage({ target: xiongyue, messageChain:[{ type:"Plain", text: `兄弟们，今天没有核酸!`}, { type: "Image", url: setu }] })
+        console.log("bingo 没有核酸发送成功")
+    }
+
 })
